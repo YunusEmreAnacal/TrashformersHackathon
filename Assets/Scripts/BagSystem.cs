2cd92e212c6621a3bcc1c101c0fb221789d1b2f1
Tunahan Derviş <8tunahandervis@gmail.com>
2024-12-14 15:11:54 +0300

    Inventory

--- Assets/Scripts/BagSystem.cs
+++ Assets/Scripts/BagSystem.cs
@@ -5,11 +5,20 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] models; // Spawnlanacak modeller
     public Transform spawnPoint; // Modellerin spawnlanacaðý nokta
 
+    private GameObject currentModel; // Halihazýrda spawnlanmýþ modeli tutacak referans
+
     public void SpawnModel(int modelIndex)
     {
         if (modelIndex >= 0 && modelIndex < models.Length)
         {
-            Instantiate(models[modelIndex], spawnPoint.position, spawnPoint.rotation);
+            // Önceki modeli yok et
+            if (currentModel != null)
+            {
+                Destroy(currentModel);
+            }
+
+            // Yeni modeli spawnla ve referansý güncelle
+            currentModel = Instantiate(models[modelIndex], spawnPoint.position, spawnPoint.rotation);
         }
         else
         {
