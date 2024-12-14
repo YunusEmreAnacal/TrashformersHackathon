fe020cf7a3738f0966e68f413f84a404ad627aed
Tunahan Derviş <8tunahandervis@gmail.com>
2024-12-14 13:36:31 +0300

    ınventory

--- Assets/Scripts/BagSystem.cs
+++ Assets/Scripts/BagSystem.cs
@@ -1,65 +1,19 @@
 using UnityEngine;
 
-public class BagSystem : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
-    public Transform bagPosition; // Çantanýn sol omuzdaki pozisyonu
-    public Transform hand; // Kullanýcýnýn elinin pozisyonu
-    public GameObject bagModel; // Çanta modeli
-    public int playerLevel = 1; // Oyuncu seviyesi
-    public int maxCapacity = 5; // Baþlangýç kapasitesi
-    private int currentCapacity = 0; 
-    private bool isBagInHand = false; 
+    public GameObject[] models; // Spawnlanacak modeller
+    public Transform spawnPoint; // Modellerin spawnlanacaðý nokta
 
-    void Start()
+    public void SpawnModel(int modelIndex)
     {
-        UpdateCapacity(); 
-        //bagModel.SetActive(false); 
-    }
-
-    void Update()
-    {
-        // El omuz pozisyonuna yakýn mý ve tutma butonu basýlý mý?
-        if (Vector3.Distance(hand.position, bagPosition.position) < 0.2f && Input.GetButton("Grab"))
+        if (modelIndex >= 0 && modelIndex < models.Length)
         {
-            TakeBag();
+            Instantiate(models[modelIndex], spawnPoint.position, spawnPoint.rotation);
         }
-    }
-
-    void TakeBag()
-    {
-        if (!isBagInHand)
+        else
         {
-            //bagModel.SetActive(true);
-            isBagInHand = true;
+            Debug.LogError("Geçersiz model indexi!");
         }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        // Garbage etiketli bir obje mi?
-        if (other.CompareTag("Garbage") && isBagInHand)
-        {
-            if (currentCapacity < maxCapacity)
-            {
-                currentCapacity++;
-                Destroy(other.gameObject); // Objeyi sahneden kaldýr
-                Debug.Log($"Çantadaki obje sayýsý: {currentCapacity}/{maxCapacity}");
-            }
-            else
-            {
-                Debug.Log("Çanta dolu! Daha fazla obje alamazsýnýz.");
-            }
-        }
-    }
-
-    public void LevelUp()
-    {
-        playerLevel++;
-        UpdateCapacity();
-    }
-
-    private void UpdateCapacity()
-    {
-        maxCapacity = playerLevel * 5; 
     }
 }
